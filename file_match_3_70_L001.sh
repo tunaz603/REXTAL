839f98231f549cb37bde2bc7b4357c7a224db9bc
Tunazzina Islam <tisla003@odu.edu>
2018-08-03 11:01:09 -0400

    Update file_match_3_70_L001.sh

--- file_match_3_70_L001.sh
+++ file_match_3_70_L001.sh
@@ -1,3 +1,3 @@
-for f in /scratch-lustre/tisla003/Original_Data/summer2018/BLAT_output/Xq_2nd/split_L001/x???; do
+for f in /scratch-lustre/tisla003/Original_Data/summer2018/BLAT_output/18p/split_L001/x???; do
   grep -n -w -f "$f" /scratch-lustre/tisla003/Original_Data/longranger/longranger_input/HG00353/HG00353_S1_L001_I1_001.fastq 
 done
